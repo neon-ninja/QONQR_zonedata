eb7d97301195f3f8db7f0e0ad445cb75d58ff0eb
Nick Young <nick.young@auckland.ac.nz>
2020-04-20 15:13:05 +1200

    calculate deltas

--- find_unique_zones.py
+++ find_unique_zones.py
@@ -4,5 +4,10 @@ import glob
 import pandas as pd
 
 df = pd.concat([pd.read_csv(f) for f in glob.glob('data/dailyzoneupdates-*.csv')], ignore_index = True)
-df = df.sort_values('LastUpdateDateUtc', ascending=False).drop_duplicates(['ZoneId'])
+df = df.sort_values('LastUpdateDateUtc', ascending=False).groupby("ZoneId").head(2)
+grouped_df = df.groupby("ZoneId")
+df["LegionDelta"] = grouped_df["LegionCount"].diff(-1)
+df["SwarmDelta"] = grouped_df["SwarmCount"].diff(-1)
+df["FacelessDelta"] = grouped_df["FacelessCount"].diff(-1)
+df = df.drop_duplicates(['ZoneId'])
 df.to_csv("data/monthly_unique_zones.csv", index=False)
