e0de77b410846f8390924f395224faf988477a06
Nick Young <nick.young@auckland.ac.nz>
2020-06-02 09:38:40 +1200

    re-add some missing deltas, prime fastly cache on push

--- update.sh
+++ update.sh
@@ -6,3 +6,4 @@ cd ..
 python3 find_unique_zones.py
 git commit -am "auto update"
 git push
+curl https://raw.githubusercontent.com/neon-ninja/QONQR_zonedata/master/data/monthly_unique_zones.csv > /dev/null
