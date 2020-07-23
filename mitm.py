c7c9a0be2bca10fc9492f3c4450113ebd07e21b8
Nick Young <nick.young@auckland.ac.nz>
2020-07-23 22:44:12 +1200

    keep a log of urls that gave new data

--- mitm.py
+++ mitm.py
@@ -9,10 +9,13 @@ import csv
 seen_ids = []
 csvwriter = None
 
+urllog = open(file="urls.txt", mode="w", buffering=1)
+
 def response(flow: http.HTTPFlow):
     global seen_ids, csvwriter
     if flow.request.pretty_url.startswith("https://api.qonqr.com/v2/Zones/Pins"):
         data = json.loads(flow.response.content)
+        has_new_data = False
         for zone in data["zones"]:
             if not csvwriter:
                 print(",".join(zone.keys()))
@@ -20,5 +23,8 @@ def response(flow: http.HTTPFlow):
                 csvwriter.writeheader()
             print(",".join(str(x or "") for x in zone.values()))
             if zone["zoneId"] not in seen_ids:
+                has_new_data = True
                 seen_ids.append(zone["zoneId"])
                 csvwriter.writerow(zone)
+        if has_new_data:
+            print(flow.request.pretty_url, file=urllog)
