aaf82b09e29d34dfcd9a042b37f07918b2404db8
Nick Young <nick.young@auckland.ac.nz>
2021-02-18 10:07:41 +1300

    switch back to authored by / commited by pattern

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ cd data
 xargs -n 1 curl -ROfLs < ../links.txt
 cd ..
 python3 find_unique_zones.py
-git -c user.name="QONQR-zonedata-bot" -c user.email="ubuntu@elevation.auckland-cer.cloud.edu.au" commit -am "auto update"
+git commit -am "auto update" --author="QONQR-zonedata-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
 curl https://raw.githubusercontent.com/neon-ninja/QONQR_zonedata/master/data/monthly_unique_zones.csv > /dev/null
