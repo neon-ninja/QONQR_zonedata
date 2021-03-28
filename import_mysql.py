0c7f82a0a42368d2dbca8e891228b4e430dec430
Nick Young <nick.young@auckland.ac.nz>
2021-03-29 12:05:40 +1300

    adjust importer script to perform incremental updates

--- import_mysql.py
+++ import_mysql.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# DB seeded with
+# LOAD DATA INFILE "AllZones.csv" INTO TABLE zones FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES (ZoneId,Description,RegionId,CountryId,UtmGrid,GridRef,ZoneControlState,@DateCapturedUtc,LegionCount,SwarmCount,FacelessCount,@LastUpdateDateUtc,Latitude,Longitude) SET DateCapturedUtc = IF(@DateCapturedUtc = '', NULL, @DateCapturedUtc), LastUpdateDateUtc = IF(@LastUpdateDateUtc = '', NULL, @LastUpdateDateUtc);
+
 import mysql.connector
 import config
 import pandas as pd
@@ -7,22 +10,43 @@ import sys
 from tqdm.auto import tqdm
 
 db = mysql.connector.connect(
-  host="localhost",
-  user=config.user,
-  password=config.password,
-  database="qonqr",
-  autocommit=True
+    host="localhost",
+    user=config.user,
+    password=config.password,
+    database="qonqr",
+    autocommit=True
 )
 
 if len(sys.argv) > 1:
     files = sys.argv[1:]
 
-cur = db.cursor()
+cur = db.cursor(dictionary=True)
 
 for f in files:
-    df = pd.read_csv(f, dtype=str)
-    df = df.fillna("NULL")
-    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
-        values = ",".join([f'"{v}"' if v != "NULL" else v for v in row.values])
-        sql = f"""REPLACE INTO zones ({','.join(row.index)}) VALUES ({values})"""
-        cur.execute(sql)
+    print(f"Loading {f}")
+    df = pd.read_csv(f)
+    df.DateCapturedUtc = pd.to_datetime(df.DateCapturedUtc)
+    df.LastUpdateDateUtc = pd.to_datetime(df.LastUpdateDateUtc)
+    print("Loaded. Fetching existing data...")
+    cur.execute(f"SELECT * FROM zones WHERE ZoneID IN ({','.join(df.ZoneId.astype(str))})")
+    existing_data = cur.fetchall()
+    print(f"{len(existing_data)} existing rows fetched")
+    updates = 0
+    for e in tqdm(existing_data):
+        match = df[df.ZoneId == e["ZoneId"]].iloc[0].to_dict()
+        if match["LastUpdateDateUtc"] > e["LastUpdateDateUtc"]:
+            print(f"Zone {match['ZoneId']}: {match['LastUpdateDateUtc']} > {e['LastUpdateDateUtc']}")
+            match["TotalCount"] = match["LegionCount"] + match["SwarmCount"] + match["FacelessCount"]
+            match["LegionDelta"] = match["LegionCount"] - e["LegionCount"]
+            match["SwarmDelta"] = match["SwarmCount"] - e["SwarmCount"]
+            match["FacelessDelta"] = match["FacelessCount"] - e["FacelessCount"]
+            match["TotalDelta"] = abs(match["LegionDelta"]) + abs(match["SwarmDelta"]) + abs(match["FacelessDelta"])
+            for k, v in match.items():
+                if pd.isnull(v):
+                    match[k] = "NULL"
+                else:
+                    match[k] = f'"{v}"'
+            sql = f"""REPLACE INTO zones ({','.join(match.keys())}) VALUES ({','.join(match.values())})"""
+            updates += 1
+            cur.execute(sql)
+    print(f"Did {updates} updates")
