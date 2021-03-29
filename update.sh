6a3571fe9332f09bb5d8ef1151e1c519edf3aa96
Nick Young <nick.young@auckland.ac.nz>
2021-03-29 23:14:28 +1300

    populate monthly_unique_zones from MySQL

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ cd data
 xargs -n 1 curl -ROfLs < ../links.txt
 cd ..
 python3 import_mysql.py data/dailyzoneupdates-*
-python3 find_unique_zones.py
+#python3 find_unique_zones.py
+sudo mysql -Be "SELECT ZoneId,Description,RegionId,CountryId,ZoneControlState,DateCapturedUtc,LegionCount,SwarmCount,FacelessCount,LastUpdateDateUtc,Latitude,Longitude,LegionDelta,SwarmDelta,FacelessDelta,TotalCount,TotalDelta FROM qonqr.zones WHERE LastUpdateDateUtc > '2021-01-01' ORDER BY TotalCount DESC" > data/monthly_unique_zones.csv
 git commit -am "auto update" --author="QONQR-zonedata-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
 curl https://raw.githubusercontent.com/neon-ninja/QONQR_zonedata/master/data/monthly_unique_zones.csv > /dev/null
