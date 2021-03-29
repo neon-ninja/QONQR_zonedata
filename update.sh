735912afdcb7a765be9fabfbb807ff42770f0646
Nick Young <nick.young@auckland.ac.nz>
2021-03-29 16:52:11 +1300

    auto-update mysql

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ git pull
 cd data
 xargs -n 1 curl -ROfLs < ../links.txt
 cd ..
+python3 import_mysql.py data/dailyzoneupdates-*
 python3 find_unique_zones.py
 git commit -am "auto update" --author="QONQR-zonedata-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
