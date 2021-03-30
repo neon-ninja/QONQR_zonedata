567f2e7aaeb7d25a70aaa5fcde1f51a546b5dc5b
Nick Young <nick.young@auckland.ac.nz>
2021-03-30 15:23:58 +1300

    only autocommit data folder and battlestats

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ cd ..
 python3 import_mysql.py data/dailyzoneupdates-*
 #python3 find_unique_zones.py
 sudo mysql -Be "SELECT ZoneId,Description,RegionId,CountryId,ZoneControlState,DateCapturedUtc,LegionCount,SwarmCount,FacelessCount,LastUpdateDateUtc,Latitude,Longitude,LegionDelta,SwarmDelta,FacelessDelta,TotalCount,TotalDelta FROM qonqr.zones WHERE LastUpdateDateUtc > '2021-01-01' ORDER BY TotalCount DESC" > data/monthly_unique_zones.csv
-git commit -am "auto update" --author="QONQR-zonedata-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
+git add data battlestats.csv
+git commit -m "auto update" --author="QONQR-zonedata-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
 curl https://raw.githubusercontent.com/neon-ninja/QONQR_zonedata/master/data/monthly_unique_zones.csv > /dev/null
