c066ddcdb876ab6bda7e963ec6f47b42da4bb2e8
Nick Young <nick.young@auckland.ac.nz>
2022-01-14 08:36:04 +1300

    import_mysql_changelog performance improvement

--- import_mysql_changelog.py
+++ import_mysql_changelog.py
@@ -19,20 +19,20 @@ if len(sys.argv) > 1:
 
 cur = db.cursor(dictionary=True)
 
-print("Fetching existing zone IDs")
-cur.execute(f"SELECT ZoneId FROM zones")
-valid_zone_ids = [r["ZoneId"] for r in cur.fetchall()]
-print(len(valid_zone_ids))
+cur.execute(f"SELECT MAX(LastUpdateDateUtc) as latest FROM changelog")
+latest = cur.fetchone()["latest"]
+print(f"Latest change recorded {latest}")
 
 keys = "ZoneId, ZoneControlState, DateCapturedUtc, LegionCount, SwarmCount, FacelessCount, LastUpdateDateUtc".split(", ")
 
 for f in files:
     print(f"Loading {f}")
     df = pd.read_csv(f)
-    df = df[df.ZoneId.isin(valid_zone_ids) & ~pd.isnull(df.LastUpdateDateUtc)]
+    df = df[~pd.isnull(df.LastUpdateDateUtc)]
     df.DateCapturedUtc = pd.to_datetime(df.DateCapturedUtc).dt.floor("s")
     df.LastUpdateDateUtc = pd.to_datetime(df.LastUpdateDateUtc).dt.floor("s")
-    print("Loaded")
+    df = df[df.LastUpdateDateUtc > latest]
+    print(f"{len(df)} rows to apply")
     updates = 0
     for i, row in tqdm(df.iterrows(), total=len(df)):
         row = {k: row[k] for k in keys}
