c49f17f53647d79370e9436c02aaff7a0594d7dd
Nick Young <nick.young@auckland.ac.nz>
2023-10-17 13:58:24 +1300

    commit changes to exchange_rates.csv

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ python3 update_battlestats.py
 python3 get_exchange_rate.py
 #python3 find_unique_zones.py
 sudo mysql -Be "SELECT ZoneId,Description,RegionId,CountryId,ZoneControlState,DateCapturedUtc,LegionCount,SwarmCount,FacelessCount,LastUpdateDateUtc,Latitude,Longitude,LegionDelta,SwarmDelta,FacelessDelta,TotalCount,TotalDelta FROM qonqr.zones WHERE DATEDIFF(CURDATE(), LastUpdateDateUtc) < 30 ORDER BY TotalCount DESC" > data/monthly_unique_zones.csv
-git add data battlestats.csv
+git add data *.csv
 git commit -m "auto update" --author="QONQR-zonedata-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
 curl https://raw.githubusercontent.com/neon-ninja/QONQR_zonedata/master/data/monthly_unique_zones.csv > /dev/null
