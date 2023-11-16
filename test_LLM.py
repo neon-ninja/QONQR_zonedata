75dd8f105654caef5cbdd3f428301de958ada0d1
Nick Young <nick.young@auckland.ac.nz>
2023-11-17 12:40:47 +1300

    SmartDatalake

--- test_LLM.py
+++ test_LLM.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3.10
 
 import pandas as pd
-from pandasai import SmartDataframe
+from pandasai import SmartDataframe, SmartDatalake
 from langchain.chat_models import ChatOpenAI
 import logging
 logging.basicConfig()
 logging.getLogger("pandasai").setLevel(logging.DEBUG)
 
-df = SmartDataframe("battlestats.csv", config={"llm": ChatOpenAI()})
-print(df.chat('Print out the top 10 Zones'))
-print(df.last_result)
-print(df.chat('Plot the top 10 Zones'))
+df = SmartDataframe("battlestats.csv", config={
+    "name": "battles",
+    "llm": ChatOpenAI(),
+    "enable_cache": False,
+    "custom_whitelisted_dependencies": ["PIL"]
+}).drop(columns=["players"])
+player_df = SmartDataframe("battlestats_players.csv", config={
+    "name": "players",
+    "llm": ChatOpenAI(),
+    "enable_cache": False,
+    "custom_whitelisted_dependencies": ["PIL"]
+})
+df = SmartDatalake([df, player_df], config={"llm": ChatOpenAI(), "enable_cache": False})
+print(df.chat('Group by player name, sum their Total Launches in battles where the country is not Atlantis, sort descending, then print the top 10 as a table'))
 print(df.last_result)
