ca0d6ae3c4a1e8eb75e199f356431ac833dcaa61
Nick Young <nick.young@auckland.ac.nz>
2025-08-14 09:24:50 +1200

    fix for new websocket version

--- mysql_websocket_server.py
+++ mysql_websocket_server.py
@@ -32,22 +32,17 @@ def query(db, query):
     print(f"""Query: {query}. Query completed in {time.time() - s}s, {len(results)} results""")
     return {"results": results}
 
-async def non_ws(path, request_headers):
-    if path != "/websocket":
-        message = unquote(path).strip("/")
-        db = create_db_connection()
-        result = query(db, message)
-        return http.HTTPStatus.OK, [("Content-Type", "application/json")], json.dumps(result).encode("utf-8")
-
-async def handle_request(websocket, path):
+async def handle_request(websocket):
     db = create_db_connection()
     async for message in websocket:
         result = query(db, message)
         await websocket.send(json.dumps(result))
 
-start_server = websockets.serve(
-    handle_request, "localhost", 8082, process_request=non_ws
-)
+async def main():
+    async with websockets.serve(
+        handle_request, "localhost", 8082
+    ) as server:
+        await server.serve_forever()
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+    asyncio.run(main())
